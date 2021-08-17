334d90fe4832d09d9ea7c8e4519c751d1d5bf172
x55aa <micrsv80@gmail.com>
2021-08-18 00:17:45 +0800

    update

--- tools/pmod3_led.sh
+++ tools/pmod3_led.sh
@@ -4,11 +4,15 @@
 
 [[ ! -e $port ]] && echo ">> Target port not exists!" && exit 0
 
+uterm_app="./uterm.py"
+[[ ! -x "$uterm_app" ]] && uterm_app="./tools/uterm.py"
+[[ ! -x "$uterm_app" ]] && echo ">> uterm.py not executable or not seen by me!" && exit 0
+
 i=1
 for ((;;)) do
 	let v=255-i
 	vv=$(printf '0x%02x' $v)
-	./uterm.py -p $port 0x10000000 $vv 2>/dev/null
+	$uterm_app -p $port 0x10000000 $vv 2>/dev/null
 	[[ "$?" != "0" ]] && break
        	sleep 0.1
 	let i=i*2
